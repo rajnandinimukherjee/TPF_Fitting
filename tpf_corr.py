Fixed the COV matrix for the T_2 interval and also in the bootstrap file. Next: finding covariance matrix between the fit parameters a, and b.

tpf_corr.py
@@ -8,12 +8,13 @@ tpf_data = np.loadtxt(tpf_file, usecols=(0,1,2,3))
 
 t_init = int(tpf_data[0,0])
 t_fin = int(tpf_data[tpf_data.shape[0]-1,0])
-T = (t_fin-t_init)+1
+T = int(t_fin)+1
 
 #print(t_init,t_fin)
 
 D = 18 # number of data points for each timeslice
 
+# 'r' stands for raw
 tpf_r = np.zeros(shape=(T,D))
 tpf_r_avg = np.zeros(T)
 
@@ -35,7 +36,7 @@ def eff_m(T,dat):
         #print(eff_m[t])
     return eff_m
 
-T_arr = np.array(list(range(T)))
+T_arr = np.arange(T)
 eff_mass = eff_m(T,tpf_r_avg)
 
 #print(eff_mass)
@@ -47,6 +48,33 @@ plt.xlabel("T")
 plt.ylabel("eff_m")
 
 #==================================================
+
+# fold data set and create COV matrix over folded set
+
+T_2 = int(T/2)
+
+# create folded data set
+# 'f' stands for folded
+tpf_f = np.zeros(shape=(T_2, D))
+tpf_f[0,:] = tpf_r[0,:]
+tpf_f_avg = np.zeros(T_2)
+tpf_f_avg[0] = tpf_r_avg[0]
+
+for t in range(1,T_2):
+    tpf_f[t,:] = (tpf_r[t,:]+tpf_r[T-t,:])/2
+    tpf_f_avg[t] = tpf_f[t,:].mean()
+    #print(t,"\t", tpf_r_avg[t], "\t", tpf_f_avg[t])
+    
+# create COV over folded data
+COV = np.zeros(shape=(T_2,T_2))
+
+for t1 in range(T_2):
+    for t2 in range(T_2):
+        COV[t1,t2] = ((tpf_f[t1,:]-tpf_f_avg[t1]).dot(tpf_f[t2,:]-tpf_f_avg[t2]))/(D*(D-1))
+
+np.savetxt("COV.csv", COV, delimiter="\t")
+
+#=================================================
     
 # correlated fit
 
@@ -55,44 +83,13 @@ fit_end = 21
 fit_interval = fit_end-fit_start
 print("fit range: ", fit_start, " - ", fit_end-1)
 
-
-# fold over set
-T_2 = int(T/2)
-#print(T)
-
-
-#folding data over fit interval
-tpf_f_fit = np.zeros(fit_interval)
-#err_f = np.zeros(T_2)
-#err_f[0] = err[0]
-
-# also fold over data set - for values 1 - 63 combine data
-data_f = np.zeros(shape=(fit_interval,2*D))
-
-for i in range (fit_interval):
-    tpf_f_fit[i] = (tpf_r_avg[i+fit_start]+tpf_r_avg[T-(i+fit_start)])/2
-    data_f[i,:] = np.append(tpf_r[i+fit_start,:], tpf_r[i+fit_start,:])
-    #err_f[i] = err[i]+err[T-i]
-    #print(tpf_f[i],err_f[i])
-
-#==================================================
-
-# calculating covariance matrix COV only in fit region
-
-COV = np.zeros(shape=(fit_interval,fit_interval))
-
-for t1 in range(fit_interval):
-    for t2 in range(fit_interval):
-        COV[t1,t2] = ((data_f[t1,:]-tpf_r_avg[t1+fit_start]).dot((data_f[t2,:]-tpf_r_avg[t2+fit_start])))/(2*D*((2*D)-1))
-        
-np.savetxt("COV.csv", COV, delimiter="\t")
-
-
 # truncating data set from both ends for fitting
 T_fit_range = T_arr[fit_start:fit_end]
+tpf_f_fit = tpf_f_avg[fit_start:fit_end]
+COV_trunc = COV[fit_start:fit_end,fit_start:fit_end]
 
 # Cholesky decomposition
-L_inv = np.linalg.cholesky(COV)
+L_inv = np.linalg.cholesky(COV_trunc)
 L = np.linalg.inv(L_inv)
 
 # fit ansatz
@@ -108,7 +105,7 @@ def LD(params):
     return L.dot(diff(params))
 
 #guess parameters:
-t_guess = int((fit_end-fit_start)/2)
+t_guess = int(fit_interval/2)
 b0 = np.abs(np.log(tpf_f_fit[t_guess-1]/tpf_f_fit[t_guess]))
 a0 = tpf_f_fit[t_guess]/np.exp(-b0*t_guess)
 #print([a0,b0])

tpf_corr_btsp.py
@@ -31,15 +31,15 @@ for t in range (T):
 K = 10000 # number of bootstrap samples
 
 #generate bootstrap samples
-btsp_samp = np.zeros(shape=(T,K,D))
-samp_avg = np.zeros(shape=(T,K))
+btsp_samp = np.zeros(shape=(K,T,D))
+samp_avg = np.zeros(shape=(K,T))
 tpf_btsp = np.zeros(T)
 
 for t in range(T):
     for k in range(K):
-        btsp_samp[t,k,:D] = random.choices(tpf_r[t,:D],k=D)
-        samp_avg[t,k] = btsp_samp[t,k,:].mean()
-    tpf_btsp[t] = btsp_samp[t,:,:].mean()
+        btsp_samp[k,t,:D] = random.choices(tpf_r[t,:D],k=D)
+        samp_avg[k,t] = btsp_samp[k,t,:].mean()
+    tpf_btsp[t] = btsp_samp[:,t,:].mean()
 
 #print(tpf_btsp)
 
@@ -67,17 +67,32 @@ plt.ylabel("eff_m")
     
 #==================================================
 
-# calculating covariance matrix COV for each bootstrap sample
+# fold over data set for each bootstrap sample
+T_2 = int(T/2)
 
-COV = np.zeros(shape=(K,T,T))
+# create folded data set
+# 'f' stands for folded
+tpf_f = np.zeros(shape=(K,T_2, D))
+tpf_f_avg = np.zeros(shape=(K,T_2))
 
 for k in range(K):
-    for t1 in range(T):
-        for t2 in range(T):
-            COV[k,t1,t2] = ((btsp_samp[t1,k,:D]-tpf_btsp[t1]).dot((btsp_samp[t2,k,:D]-tpf_btsp[t2])))/(D*(D-1))
-        
-#np.savetxt("COV.csv", COV, delimiter="\t")
+    tpf_f[k,0,:] = btsp_samp[k,0,:]
+    tpf_f_avg[k,0] = samp_avg[k,0]
+    for t in range(1,T_2):
+        tpf_f[k,t,:] = (btsp_samp[k,t,:]+btsp_samp[k,T-t,:])/2
+        tpf_f_avg[k,t] = tpf_f[k,t,:].mean()
 
+#==================================================
+
+# calculating covariance matrix COV for each bootstrap sample
+
+COV = np.zeros(shape=(K,T_2,T_2))
+
+for k in range(K):
+    for t1 in range(T_2):
+        for t2 in range(T_2):
+            COV[k,t1,t2] = ((tpf_f[k,t1,:]-tpf_f_avg[k,t1]).dot(tpf_f[k,t2,:]-tpf_f_avg[k,t2]))/(D*(D-1))
+        
 #==================================================
     
 # correlated fit on each bootstrap sample
@@ -86,6 +101,7 @@ T_2 = int(T/2)
 
 fit_start = 7
 fit_end = 21
+fit_interval = fit_end-fit_start
 
 print("fit range: ", fit_start, " - ", fit_end-1)
 
@@ -94,20 +110,11 @@ T_fit_range = T_arr[fit_start:fit_end]
 COV_trunc = COV[:,fit_start:fit_end, fit_start:fit_end]
 
 params_dist = np.zeros(shape=(K,2))
+tpf_f_fit = np.zeros(fit_interval)
 
 for k in range(K):
-    
-    # fold over each bootstrap sample
-    tpf_f = np.zeros(T_2)
-    tpf_f[0] = samp_avg[0,k]
-    #err_f = np.zeros(T_2)
-    #err_f[0] = err[0]
-
-    for i in range (1,T_2):
-        tpf_f[i] = (samp_avg[i,k]+samp_avg[T-i,k])/2
         
-    tpf_f_fit = tpf_f[fit_start:fit_end]
-    
+    tpf_f_fit[:] = tpf_f_avg[k,fit_start:fit_end]
     
     # Cholesky decomposition
     L_inv = np.linalg.cholesky(COV_trunc[k,:,:])
@@ -135,8 +142,6 @@ for k in range(K):
 
     res = least_squares(LD, [a0,b0])
     params_dist[k,:] = res.x
-    #chi_sq = ((LD(res.x)).T).dot(LD(res.x))
-    #print(chi_sq)
     
 #====================================================
     
@@ -147,6 +152,7 @@ a = a_data.mean()
 a_bins = np.arange(min(a_data), max(a_data),(max(a_data)-min(a_data))/500)
 plt.hist(a_data, bins=a_bins, alpha=0.5)
 plt.axvline(a, color='k')
+plt.title("a")
 
 plt.figure()
 b_data = params_dist[:,1]
@@ -154,8 +160,9 @@ b = b_data.mean()
 b_bins = np.arange(b-0.05,b+0.05,0.1/500)
 plt.hist(b_data, bins=b_bins, alpha=0.5)
 plt.axvline(b, color='k')
+plt.title("b")
 
-print("a = ", a, "\nb = ", b)
+print("a = ", a, " +/- ", np.std(a_data), "\nb = ", b, " +/- ", np.std(b_data))
 
 plt.show()
 